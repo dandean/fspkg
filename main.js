e813e83b4a52bd5e4835e81ea2d86215348eaf3d
dandean <danwdean@gmail.com>
2011-12-16 10:07:32 -0800

    Refactoring: Builder and SyncBuilder are now seperate classes.

--- main.js
+++ main.js
@@ -1,121 +1,8 @@
 var fs = require('fs');
 var path = require('path');
 var util = require('util');
-
-function escape(str) {
-  return str.replace(/([.*+?^=!:${}()|[\]\/\\])/g, '\\$1');
-}
-
-/**
- * class Builder
- *
- * new Builder([options])
- * - options (Object): Options to use for the `Builder` instance.
- *
- * Creates an instance of `Builder` with the given options.
-**/
-function Builder(options) {
-  options = options || {};
-  options.filter = Builder.DefaultFilter;
-  options.format = 'js';
-  
-  this.options = options;
-}
-
-
-//
-// INSTANCE PROPERTIES
-//
-
-/**
- * Builder#build(root, cb) -> undefined
- * - root (String): The root path of the package.
- * - cb (Function(e, result)): Callback function which is given the result.
- *
- * Processes the file-system package at `root`.
-**/
-Builder.prototype.build = function(root, cb) {
-};
-
-/**
- * Builder#buildSync(root) -> String
- * - root (String): The root path of the package.
- *
- * Processes and returns the file-system package at `root`.
-**/
-Builder.prototype.buildSync = function(root) {
-  var t = this;
-  var result = {};
-  
-  root = path.resolve(root);
-  
-  var walk = function(current) {
-    var stats = fs.statSync(current);
-    
-    if (stats.isFile() && t.options.filter(current)) {
-
-      var key = current.replace(new RegExp('^' + escape(root)), '').replace(/^\//, '');
-      var value = fs.readFileSync(current, 'utf8');
-      result[key] = value;
-
-    } else if (stats.isDirectory()) {
-      
-      fs.readdirSync(current).forEach(function(name) {
-        walk(path.join(current, name))
-      });
-      
-    }
-  };
-  
-  walk(root);
-  
-  switch (this.options.format) {
-    case 'js':
-      return 'module.exports = ' + util.inspect(result);
-    case 'json':
-      return JSON.stringify(result);
-  }
-  
-  return result;
-};
-
-
-//
-// CLASS PROPERTIES
-//
-
-/**
- * Builder.DefaultFilter(path) -> Boolean
- * - path (String): The path to be tested.
- *
- * Return `true` if the path should be included in the result.
-**/
-Builder.DefaultFilter = function(path) {
-  return path
-         && path.match(/\.(mustache|html|htm|tpl|js)$/i)
-         && path.match(/\/(\.git|node_modules)\//) === null;
-};
-
-/**
- * namespace Builder.Processor
- *
- * A collection of async file processors. Result is passed to the callback.
-**/
-Builder.Processor = {
-  text: function(path, cb) {
-  }
-};
-
-/**
- * namespace Builder.SyncProcessor
- *
- * A collection of synchronous file processors. Result is returned from each function.
-**/
-Builder.SyncProcessor = {
-  text: function(path) {
-  }
-};
-
+var Builder = require('./lib/builder');
+var SyncBuilder = require('./lib/sync-builder');
 
 //
 // MODULE EXPORT
@@ -135,3 +22,4 @@ var fspkg = module.exports = function(path, options, cb) {
 }
 
 fspkg.Builder = Builder;
+fspkg.SyncBuilder = SyncBuilder;
--- test/test.js
+++ test/test.js
@@ -7,45 +7,16 @@ describe('Module', function(){
   });
   
   it('should should have a `Builder` property which is a function', function(){
-    assert.ok(fspkg.Builder instanceof Function);
+    assert.ok(fspkg.SyncBuilder instanceof Function);
   });
   
-  var pkgr = new fspkg.Builder();
-  var result = pkgr.buildSync('./');
-
+  var pkgr = new fspkg.SyncBuilder();
+  var result = pkgr.build('./');
+  
+  // console.log(result);
+  
   // require('fs').writeFileSync('./test2.js', result);
   // console.log(require('../test2.js')['main.js']);
   
   // console.log(result);
-   
-  // it('should retain a reference to the raw input', function() {
-  //   assert.ok(Object.prototype.toString.call(result.rawInput) === '[object String]');
-  // });
-  // 
-  // it('should convert integer-like values to a Number', function() {
-  //   // Zero
-  //   assert.ok(Object.prototype.toString.call(result['Channel statistics'].Alpha.skewness) === '[object Number]');
-  // 
-  //   // Negative
-  //   assert.ok(Object.prototype.toString.call(result['Channel statistics'].Alpha.kurtosis) === '[object Number]');
-  // });
-  // 
-  // it('should convert float-like values to a Number', function() {
-  //   // Zero
-  //   assert.ok(Object.prototype.toString.call(result['Channel statistics'].Blue.skewness) === '[object Number]');
-  // 
-  //   // Negative
-  //   assert.ok(Object.prototype.toString.call(result['Channel statistics'].Blue.kurtosis) === '[object Number]');
-  // });
-  // 
-  // it('should convert property names to camelCase when specified', function() {
-  //   assert.ok(Object.keys(result).indexOf('Image statistics') > -1);
-  //   result = reader(input, true);
-  //   assert.ok(Object.keys(result).indexOf('imageStatistics') > -1);
-  // });
-  // 
-  // it('should extract `width` and `height` properties from `geometry` property', function() {
-  //   assert.ok(Object.keys(result).indexOf('width') > -1);
-  //   assert.ok(Object.keys(result).indexOf('height') > -1);
-  // });
 });
